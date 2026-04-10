b3a9d8db555f1c9b187f9203763d5d034f8f18d7
dekpaa <syifaaadilla16@gmail.com>
2026-04-10 08:53:12 +0800

    test dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,25 +1,3 @@
-// app/dashboard/page.tsx
-'use client'
-
-import { useRouter } from 'next/navigation'
-import { useEffect } from 'react'
-
-export default function DashboardPage() {
-  const router = useRouter()
-  
-  useEffect(() => {
-    const userRole = 'admin'
-    
-    if (userRole === 'admin') {
-      router.push('/calendar')
-    } else {
-      router.push('/calendar')
-    }
-  }, [router])
-  
-  return (
-    <div>
-      <p>Redirecting to calendar...</p>
-    </div>
-  )
+export default function Page() {
+  return <div>Dashboard OK</div>
 }
